5981cc6aaefe09034f531e5bb83c6c8364f92095
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2023-01-31 20:13:46 +0100

    added the signup and signin pages with landing page

--- build/js/main.js
+++ build/js/main.js
@@ -1,23 +1,38 @@
-let sidebar = document.querySelector('.sidebar')
-let closeBtn = document.querySelector('#btn')
-let searchBtn = document.querySelector('.bx-search')
+// let sidebar = document.querySelector('.sidebar')
+// let closeBtn = document.querySelector('#btn')
+// let searchBtn = document.querySelector('.bx-search')
 
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('open')
-  menuBtnChange(); // calling the function(optional)
-})
+// closeBtn.addEventListener('click', () => {
+//   sidebar.classList.toggle('open')
+//   menuBtnChange(); // calling the function(optional)
+// })
 
-searchBtn.addEventListener('click', () => {
-  // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle('open')
-  menuBtnChange(); // calling the function(optional)
-})
+// searchBtn.addEventListener('click', () => {
+//   // Sidebar open when you click on the search iocn
+//   sidebar.classList.toggle('open')
+//   menuBtnChange(); // calling the function(optional)
+// })
 
-// following are the code to change sidebar button(optional)
-function menuBtnChange () {
-  if (sidebar.classList.contains('open')) {
-    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right') // replacing the iocns class
-  } else {
-    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu') // replacing the iocns class
+// // following are the code to change sidebar button(optional)
+// function menuBtnChange () {
+//   if (sidebar.classList.contains('open')) {
+//     closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right') // replacing the iocns class
+//   } else {
+//     closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu') // replacing the iocns class
+//   }
+// }
+const initApp = () => {
+  const hamburgerBtn = document.getElementById('hamburger-button')
+  const mobileMenu = document.getElementById('mobile-menu')
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden')
+    mobileMenu.classList.toggle('flex')
+    hamburgerBtn.classList.toggle('toggle-btn')
   }
+
+  hamburgerBtn.addEventListener('click', toggleMenu)
+  mobileMenu.addEventListener('click', toggleMenu)
 }
+
+document.addEventListener('DOMContentLoaded', initApp)
